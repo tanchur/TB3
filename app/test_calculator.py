38be0fb6f77b0d533729c2f169885bfe66805855
tanchur <tkanchyyr@mail.ru>
2025-10-29 15:25:05 +0700

    my commit

--- app/test_calculator.py
+++ app/test_calculator.py
@@ -4,7 +4,7 @@ import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from calculator import MortgageCalculator
+from app.calculator import MortgageCalculator
 
 
 class TestMortgageCalculator(unittest.TestCase):
